5c1aaa4981ea316dc70b8fe0e759c0180d82383d
Jeff Katz <kraln@kraln.com>
2014-12-07 01:22:01 +0100

    It's a game!

---
+++ item.js
@@ -0,0 +1,82 @@
+var item_exit = {
+  avatar: ">",
+  description: "the exit. Do you have everything?",
+  movable: false,
+  havable: false,
+};
+
+var item_flashlight = {
+  avatar: "F",
+  description: "bright flashlight",
+  movable: false,
+  havable: true,
+  legendary: true,
+};
+
+var item_ladder = {
+  avatar: "L",
+  description: "wooden ladder",
+  movable: false,
+  havable: true,
+  legendary: true,
+};
+
+function item_collide(item)
+{
+  if (item.havable) 
+  {
+    messages.push("There is a " + item.description + " here. You can pick it up.");
+  } else {
+    messages.push("You see " + item.description);
+  }
+
+  interactable.push(item);
+}
+
+function item_think(item)
+{
+  /* Stuff common to all items */
+  if (item.x == player.x && item.y == player.y)
+  {
+    item_collide(item);
+  } 
+}
+
+var draw_entity = function() {};
+var genRand = function(x, y) {};
+
+function item_draw(item)
+{
+  draw_entity(item.avatar, item.x, item.y);
+}
+
+function item_spawn(room, item)
+{
+  /* add the item to the list of tracked entities */
+
+  /* First clone */
+  /* XXX: Erases all functions */
+  var i = JSON.parse(JSON.stringify(item)); 
+
+  /* all items think and draw the same way */
+  i.draw = item_draw;
+  i.think = item_think;
+  i.type = "item";
+
+  /* put item into room */
+  i.x = room.x - (room.w/2) + genRand(1, room.w-2);
+  i.y = room.y - (room.h/2) + genRand(1, room.h-2);
+  
+  /* give the item a reference to the room */
+  i.r = room;
+
+  /* Let the room know it has this item */
+  room.i.push(i);
+
+  /* Push the item into the entity pool */
+  entities.push(i);
+
+  return i;
+}
+
+
--- main.js
+++ main.js
@@ -8,12 +8,194 @@ var screen_y = 24;
 var cam_x = world_x/2; /* The "camera" position */
 var cam_y = world_y/2; 
 var rooms = []; /* track room centers */
+var entities = []; /* track things */
+var turn = 0;
+var messages = [];
+var blocked_on_message = false;
+var interactable = [];
+var game_won = false;
 
 /* Special characters */
 var block = '█';
 
 /* HTML handle */
 var handle = document.getElementById("playarea");
+
+var player = {
+  x: world_x/2,
+  y: world_y/2,
+  pos: { x: world_x/2, y: world_y/2 },
+  inventory: [],
+  moveUp: function() {
+    if (world[player.x][player.y + 1] == ' ')
+      player.y++;   
+      end_turn(); 
+  },
+  moveDown: function() {
+    if (world[player.x][player.y - 1] == ' ')
+      player.y--;
+      end_turn(); 
+  },
+  moveLeft: function() {
+    if (world[player.x + 1][player.y] == ' ')
+      player.x++;
+      end_turn(); 
+  },
+  moveRight: function() {
+    if (world[player.x - 1][player.y] == ' ')
+      player.x--;  
+      end_turn(); 
+  },
+  pickUp: function(thing) {
+    player.inventory.push(thing);
+    
+    interactable.splice(interactable.indexOf(thing), 1);
+    entities.splice(entities.indexOf(thing), 1);
+  },
+  interact: function() {
+    /* if nothing, then nothing */
+    if (!interactable.length)
+    {
+      messages.push("There's nothing here but dusty floor.");
+      return;
+    }
+
+    for (var i = 0; i < interactable.length; i++)
+    {
+      var thing = interactable[i];
+      if (thing.type == "item")
+      {
+        if (thing.havable) {
+          messages.push("You pick up the " + thing.description + ".");
+          if (thing.legendary) 
+          {
+            messages.push("Ooooooh!");
+          }
+          player.pickUp(thing);
+          continue;
+        } else {
+          /* Can I interact with it? */
+          if (thing.interact)
+          {
+            thing.interact();
+          } else {
+            messages.push("You can't play with the " + thing.description + ".");
+          }
+        }
+
+      }
+
+    }
+    
+  },
+  printInventory: function()
+  {
+    if (!player.inventory.length)
+    {
+      messages.push("You don't have any inventory.");
+      return;
+    }
+    for (var i = 0; i < player.inventory.length; i++)
+    {
+      messages.push("You have " + player.inventory[i].avatar + ", a " + player.inventory[i].description + ".");
+    }
+  },
+  draw: function()
+  {
+    player.pos = draw_entity("@", player.x, player.y);
+  }
+};
+
+/* win the game! */
+function win_game()
+{
+  alert("You have conquered the whole game, and the screenings are complete. Congratulations!\n\nTurns: " + turn);
+  game_won = true;
+  messages.push("Do you want your possessions identified? (Reload to replay!)");
+}
+
+/* give the other entities a chance to think */
+function end_turn()
+{
+  /* clear the list of interactable things */
+  interactable = [];
+
+  /* make everythink think */
+  for (var i = 0; i < entities.length; i++)
+  {
+    entities[i].think(entities[i]);
+  }
+  
+  /* Increment the turn counter */
+  turn++; 
+}
+
+/* messages will be added by events in the game world */
+function draw_messages()
+{
+  if (messages.length == 0)
+  {
+    return;
+  }
+
+  blocked_on_message = true;
+  var message = messages.pop();
+  if (message.length > 74)
+  {
+    messages.push(message.substring(74,message.length));
+    for (var i = 0; i < message.length && i < 74; i++)
+    {
+      scr[79-i][0] = message.charAt(i);
+    }
+    scr[79-i++][0] = "-";
+    scr[79-i++][0] = "m";
+    scr[79-i++][0] = "o";
+    scr[79-i++][0] = "r";
+    scr[79-i++][0] = "e";
+    scr[79-i++][0] = "-";
+  } else {
+    for (var i = 0; i < message.length && i < 74; i++)
+    {
+      scr[79-i][0] = message.charAt(i);
+    }
+
+    i = 74;
+    scr[79-i++][0] = "-";
+    scr[79-i++][0] = "o";
+    scr[79-i++][0] = "k";
+    scr[79-i++][0] = "a";
+    scr[79-i++][0] = "y";
+    scr[79-i++][0] = "-";
+  }
+}
+
+/* Used by entities to figure out if they need to draw or not */
+function draw_entity(chr, x, y)
+{
+  screen_minx = cam_x - (screen_x / 2); 
+  screen_maxx = cam_x + (screen_x / 2) - 1;
+  screen_miny = cam_y - (screen_y / 2);
+  screen_maxy = cam_y + (screen_y / 2) - 1;
+
+  if (x < screen_minx || x > screen_maxx)
+    return { x: (x - screen_minx), y: (y - screen_miny) }
+
+  if (y < screen_miny || y > screen_maxy)
+    return { x: (x - screen_minx), y: (y - screen_miny) }
+
+  scr[x - screen_minx][y - screen_miny] = chr; 
+  return { x: (x - screen_minx), y: (y - screen_miny) }
+}
+
+/* called every time the game world is drawn */
+function entities_draw()
+{
+  for (var i = 0; i < entities.length; i++)
+  {
+    entities[i].draw(entities[i]);
+  }
+  player.draw();
+}
 
 /* Set up the container for the screen */
 function init_screen()
@@ -102,6 +284,19 @@ function fill(x1, y1, x2, y2, chr)
   }
 
   /* normal case */
+  if (x1 > x2)
+  {
+    var temp = x1;
+    x1 = x2;
+    x2 = temp;
+  }
+
+  if (y1 > y2)
+  {
+    var temp = y1;
+    y1 = y2;
+    y2 = temp;
+  }
 
   for (var x = x1; x < x2; x++)
     for (var y = y1; y < y2; y++)
@@ -115,8 +310,8 @@ function create_room(x, y, w, h)
 {
   console.log("Entered CREATE_ROOM (" + x + ", " + y + ", " + w + ", " + h + ")");
 
-  /* Check if occupied */
-  var ret = check_empty(x-w/2, y-h/2, x+w/2, y+h/2);
+  /* Check if already occupied, including border */
+  var ret = check_empty(x-w/2-1, y-h/2-1, x+w/2+1, y+h/2+1);
   if (!ret) return false;
 
   /* Create room */
@@ -125,12 +320,11 @@ function create_room(x, y, w, h)
   var r = genRand(0, 32767);
 
   /* Track the room for the future */
-  var roomId = rooms.push({x: x, y: y, w: w, h: h, rand: r, p: null, c: []}) - 1;
+  var roomId = rooms.push({x: x, y: y, w: w, h: h, rand: r, p: null, i: [], c: []}) - 1;
 
   /* Blank out the space */
   ret = fill(x-w/2, y-h/2, x+w/2, y+h/2, ' ');
 
-  /* TODO: Add random things? */
   return roomId;
 }
 
@@ -251,7 +445,7 @@ function init_world()
   var cur_room;
   var iter = 0;
 
-  while (iter < 80)
+  while (iter < 125)
   {
     cur_room = rooms[0];
     iter_create_room(cur_room);
@@ -261,10 +455,42 @@ function init_world()
   /* Recursively connect rooms */
   connect_rooms(cur_room);
 
-  /* Create entrance, exit, and other special rooms */
+  /* Put exit in starting room */
+  var exit = item_spawn(rooms[0], item_exit);
+  exit.interact = function() {
+    var has_ladder = false;
+    for (var i = 0; i < player.inventory.length; i++)
+    {
+      if (player.inventory[i].avatar == "L")
+      {
+        has_ladder = true;
+        break;
+      }    
+    }
+    if (has_ladder) {
+      win_game();
+    } else {
+      messages.push("Come back when you've found the ladder...");
+    }
+  };
 
+  
+  /* Put flashlight nearby */
+  item_spawn(rooms[genRand(2,3)], item_flashlight);
 
+  /* In last 50% of rooms */
+  /* Spawn goal items */
+  item_spawn(rooms[genRand(Math.floor(rooms.length/2),rooms.length)], item_ladder);
+  /* Spawn bosses */
+
+  /* Everywhere */
+  /* Spawn regular enemies */
+  /* Spawn regular items */
+  
   world_to_canvas();
+  
+  messages.push("Welcome to Rougish. It's your thirteenth birthday, and by the rules of thevillage you must undergo several screenings before you're allowed to join society as an adult. You've already aced the written and verbal screeningsThere's just the small matter of the game screening. A man yells at you:  Come back when you've cleared the entire game... You'll need a ladder, andyou'll need a flashlight. Hope to god grumpus doesn't find you. Good luck!");
+
   console.log("Exited INIT_WORLD");
 }
 
@@ -274,8 +500,8 @@ function draw_screen()
   var holder = "";
 
   /* Draw each character */
-  for (y = 0; y < screen_y; y++)
-    for (x = 0; x < screen_x; x++)
+  for (y = screen_y-1; y >= 0; y--)
+    for (x = screen_x-1; x >= 0; x--)
     {
       holder = holder + scr[x][y];
     }
@@ -313,13 +539,12 @@ function copy_world_to_screen(x,y)
   x = x - (screen_x / 2);
   y = y - (screen_y / 2);
 
-  for (dy = 0; dy < screen_y; dy++)
-    for (dx = 0; dx < screen_x; dx++)
+  for (dy = screen_y - 1; dy >= 0; dy--)
+    for (dx = screen_x - 1; dx >= 0; dx--)
     {
       scr[dx][dy] = world[x + dx][y + dy];
     }
 
-  /* TODO: Copy entities */
   console.log("Exited COPY_WORLD_TO_SCREEN");
 }
 
@@ -359,9 +584,68 @@ function world_to_canvas()
 
 function refresh()
 {
+  if (player.x - cam_x > 20)
+  {
+    cam_x++;
+  }
+
+  if (cam_x - player.x > 20)
+  {
+    cam_x--;
+  }
+
+  if (player.y - cam_y > 5)
+  {
+    cam_y++;
+  }
+
+  if (cam_y - player.y > 5)
+  {
+    cam_y--;
+  }
+
   copy_world_to_screen(cam_x, cam_y);
+  entities_draw();
+  draw_messages();
   draw_screen();
+  document.title = "rougish - turn " + turn;
 }
+
+/* bind the input handler */
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+  e = e || window.event;
+
+  if (game_won == true)
+    return;
+
+  if (blocked_on_message == true && e.keyCode == '13')
+  {
+    blocked_on_message = false;
+  } else if (blocked_on_message == true) {
+    return;
+  } 
+
+         if (e.keyCode == '38' || e.keyCode == '75') { /* up */
+    player.moveUp();
+  } else if (e.keyCode == '40' || e.keyCode == '74') { /* down */
+    player.moveDown();
+  } else if (e.keyCode == '37' || e.keyCode == '72') { /* left */
+    player.moveLeft();
+  } else if (e.keyCode == '39' || e.keyCode == '76') { /* right */
+    player.moveRight();
+  } else if (e.keyCode == '73') { /* i */
+    player.printInventory();
+  } else if (e.keyCode == '190') { /* . */
+    player.interact();
+  } else {
+    console.log(e.keyCode);
+  }
+
+  refresh();
+}
+
 
 init_screen();
 init_world();
